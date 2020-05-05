2f6e65cf67023aa3f93176ad7fa004464391ee8b
furuhama <furuhama@users.noreply.github.com>
2020-05-05 23:55:03 +0900

    Refactor d

--- abc164/src/bin/d.rs
+++ abc164/src/bin/d.rs
@@ -1,39 +1,28 @@
 use proconio::{input, marker::Chars};
-use num::BigUint;
+
+macro_rules! echo {
+    ($($e:expr),+) => ( { $(println!("{}", $e))+ } );
+}
 
 fn main() {
     input! {
         s: Chars,
     }
 
-    let len = s.len();
+    const MOD: usize = 2019;
+    let mut dp = vec![0usize; MOD];
+    dp[0] = 1;
+    let mut pow = 1;
+    let mut now = 0;
+    let mut ans = 0;
 
-    if len < 4 {
-        println!("0");
-        return;
+    for digit in s.iter().rev() {
+        let digit = digit.to_digit(10).unwrap() as usize;
+        now = (now + pow * digit) % MOD;
+        ans += dp[now];
+        dp[now] += 1;
+        pow = 10 * pow % MOD;
     }
 
-    let mut mod_vec: Vec<Vec<usize>> = vec![Vec::new(); 2019];
-    let mut rest: usize = 0;
-    let modulus = BigUint::from(2019_usize);
-
-    mod_vec[0].push(0);
-    for i in 0..len {
-        let digit = s[len - i - 1].to_digit(10).unwrap() as usize;
-        let pow = BigUint::from(10_usize).modpow(&BigUint::from(i), &modulus);
-        rest = (rest + digit * pow.to_u32_digits()[0] as usize) % 2019;
-        mod_vec[rest].push(i);
-    }
-
-    let mut count = 0;
-
-    for e in &mod_vec {
-        if e.len() <= 1 {
-            continue;
-        }
-
-        count += e.len() * (e.len() - 1) / 2;
-    }
-
-    println!("{}", count);
- }
+    echo!(ans);
+}
