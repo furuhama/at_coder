83df1a95d9d0181163b7a4926c416940b9b4eb3b
furuhama <furuhama@users.noreply.github.com>
2020-06-03 00:26:19 +0900

    e

--- abc169/src/bin/e.rs
+++ abc169/src/bin/e.rs
@@ -10,5 +10,23 @@ macro_rules! echo {
 }
 
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        ab: [(usize, usize); n],
+    }
+
+    let mut mins = ab.iter().map(|e| e.0).collect::<Vec<_>>();
+    mins.sort();
+    let mut maxs = ab.iter().map(|e| e.1).collect::<Vec<_>>();
+    maxs.sort();
+
+    if n % 2 == 0 {
+        let min_median = mins[n / 2 - 1] + mins[n / 2];
+        let max_median = maxs[n / 2 - 1] + maxs[n / 2];
+        echo!(max_median - min_median + 1);
+    } else {
+        let min_median = mins[(n + 1) / 2 - 1];
+        let max_median = maxs[(n + 1) / 2 - 1];
+        echo!(max_median - min_median + 1);
+    }
 }
