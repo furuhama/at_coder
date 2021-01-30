d1017f28d93ac6073a7e068565f0d69e5195a4e1
furuhama <furuhama@users.noreply.github.com>
2021-01-30 23:32:46 +0900

    c

--- abc173/src/bin/c.rs
+++ abc173/src/bin/c.rs
@@ -10,5 +10,39 @@ macro_rules! echo {
 }
 
 fn main() {
-    unimplemented!();
+    input! {
+        h: usize,
+        w: usize,
+        k: usize,
+        b: [Chars; h],
+    };
+
+    let board: Vec<Vec<bool>> = b
+        .into_iter()
+        .map(|col| {
+            col.into_iter()
+                .map(|c| if c == '#' { true } else { false })
+                .collect()
+        })
+        .collect();
+
+    let mut ans = 0;
+    for i in 0..(1 << h) {
+        for j in 0..(1 << w) {
+            let mut cnt = 0;
+            for (y, row) in board.iter().enumerate() {
+                for (x, &b) in row.into_iter().enumerate() {
+                    // (i >> y) & 1) & ((j >> x) & 1 は塗らない場合に 1 になる
+                    if ((i >> y) & 1) & ((j >> x) & 1) == 1 && b {
+                        cnt += 1;
+                    }
+                }
+            }
+            if cnt == k {
+                ans += 1;
+            }
+        }
+    }
+
+    echo!(ans);
 }
